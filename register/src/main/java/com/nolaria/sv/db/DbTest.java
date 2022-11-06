af754fefafe40b272480ffa6d210b339824f7f03
Mark Norton <markjnorton@gmail.com>
2022-11-06 07:10:39 -0500

    Added DbTest and search - for real this time!

---
+++ register/src/main/java/com/nolaria/sv/db/DbTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.nolaria.sv.db;
+
+/**
+ * @author markj
+ *
+ */
+public class DbTest {
+
+	/**
+	 * A test suite for the Nolaria DB classes.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String dbClass = null;
+		String dbMethod = null;
+		try {
+			
+			//	PageId Tests
+			dbClass = "PageId";
+			
+			dbMethod = "getPage()";
+			
+			;
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
